eb786e60e7594cadbca59bcada080c6129eea5ae
Krish Pandya <macbookpro@Krishs-MacBook-PRO-M5-PRO.local>
2026-09-16 23:50:12 -0600

    Complete Assignment 0

---
+++ Assignment0/app/src/main/java/com/example/assignment0/MainActivity.kt
@@ -0,0 +1,145 @@
+package com.example.assignment0
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.assignment0.ui.theme.Assignment0Theme
+import kotlin.random.Random
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            Assignment0Theme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+
+                    ScreenView(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ScreenView(
+    modifier: Modifier = Modifier,
+) {
+    var YESNO by remember {
+        mutableStateOf<Boolean?>(null)
+    }
+
+    var count by remember {
+        mutableStateOf(0)
+    }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        if (YESNO == false)
+            Text("NO")
+        else if (YESNO == true)
+            Text("YES")
+        else
+            Text("Make a decision")
+
+        Spacer(
+            modifier = Modifier.height(20.dp)
+        )
+
+        Row(
+            modifier = Modifier,
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Button(
+                modifier = Modifier.padding(10.dp),
+                onClick = {
+                    count++
+
+                    val random = Random.nextDouble()
+
+                    if (random < 0.5)
+                        YESNO = true
+                    else
+                        YESNO = false
+                }
+            ) {
+                Text("Ok! :)")
+            }
+
+            Button(
+                modifier = Modifier.padding(10.dp),
+                onClick = {
+                    count++
+
+                    val random = Random.nextDouble()
+
+                    if (random < 0.25)
+                        YESNO = true
+                    else
+                        YESNO = false
+                }
+            ) {
+                Text("Meh")
+            }
+
+            Button(
+                modifier = Modifier.padding(10.dp),
+                onClick = {
+                    count++
+
+                    val random = Random.nextDouble()
+
+                    if (random < 0.10)
+                        YESNO = true
+                    else
+                        YESNO = false
+                }
+            ) {
+                Text("Nah :(")
+            }
+        }
+
+        Spacer(
+            modifier = Modifier.height(20.dp)
+        )
+
+        Text("Count: $count")
+
+        Spacer(
+            modifier = Modifier.height(20.dp)
+        )
+
+        Text("Student ID: 1872629")
+        Text("CCID: pandya3")
+    }
+}
